1ba39856974e63331e86ed164a47d5314023ce42
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-03-22 21:13:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function insert_Row() {
     //Write your code here
+	
+
+    var t1 = document.createElement("TD");
+    var text1 = document.createTextNode("New Cell1");
+    t1.appendChild(text1);
+    var t2=document.createElement("TD");
+    var text2 = document.createTextNode("New Cell2");
+    t2.appendChild(text2);
+   
+    let  tr1=document.createElement("tr");
+    tr1.appendChild(t1);
+    tr1.appendChild(t2);
+    let Table=document.getElementById("sampleTable")
+    Table.insertBefore(tr1, Table.children[0]);
+
   
   
 }
